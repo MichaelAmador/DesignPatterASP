f2e5afeb6797af2109402f532a3e1161cc839535
Michael Amador Centeno <57235798+MichaelAmador@users.noreply.github.com>
2022-11-07 20:50:34 -0600

    Add project files - Singleton

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = Singleton.Log.Instance;
+            log.Save("This is a fucking test!");
+            log.Save("This is a fucking test2!");
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Log.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    public class Log
+    {
+        private readonly static Log _instance = new Log();
+        private string _path = "myText.txt";
+
+        public static Log Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        private Log()
+        {
+
+        }
+
+        public void Save(string text)
+        {
+            File.AppendAllText(_path, text + Environment.NewLine);
+        }
+
+    }
+}
